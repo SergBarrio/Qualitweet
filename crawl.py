b951a058a14fb46918c0decb4989477ac66069e6
SergBarrio <serg.barrio@gmail.com>
2012-12-04 00:15:54 -0600

    Added hits.py
    You now use:
    python hits.py 'search terms'
    Without apostrophes.

--- crawl.py
+++ crawl.py
@@ -18,6 +18,7 @@ queryoauth = OAuth1(client_key, client_secret, resource_owner_key, resource_owne
 
 # build corpus of 40 tweets each from the found users
 page = 1
+f = open('test.json', 'w')
 while (page < 5):
 	# This will get 20 (the max) users on one page
 	url = u'https://api.twitter.com/1/users/search.json?q=' + '%20'.join(sys.argv[1:]) + '&page=' + repr(page) + '&per_page=20'
@@ -29,5 +30,5 @@ while (page < 5):
 		url = u'https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + user['screen_name'] + '&count=40'
 		corpus_r = requests.get(url, auth=queryoauth)
 		for tweet in corpus_r.json:
-			print tweet
+			f.write( str(tweet)+'\n' )
 	page+=1
--- hits.py
+++ hits.py
@@ -4,10 +4,44 @@ import sys
 import ast
 import numpy as np
 from numpy import linalg as LA
+import requests
+from requests.auth import OAuth1
+import os
 
+# This reads tweets from Twitter's streaming API and writes them to a file.
+# See Twitter's documentation here:
+# https://dev.twitter.com/docs/api/1/post/statuses/filter
+# You will need to change the SCREEN_NAME and PASSWORD below.
+
+def crawl():
+    client_key = u''
+    client_secret = u''
+    resource_owner_key = u''
+    resource_owner_secret = u''
+    
+    queryoauth = OAuth1(client_key, client_secret, resource_owner_key, resource_owner_secret, signature_type='query')
+    
+    # build corpus of 40 tweets each from the found users
+    page = 1
+    f = open('corpus.json', 'w')
+    while (page < 5):
+        # This will get 20 (the max) users on one page
+        url = u'https://api.twitter.com/1/users/search.json?q=' + '%20'.join(sys.argv[1:]) + '&page=' + repr(page) + '&per_page=20'
+    
+        r = requests.get(url, auth=queryoauth)
+        
+        for user in r.json:
+            # print user['screen_name']
+            url = u'https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + user['screen_name'] + '&count=40'
+            corpus_r = requests.get(url, auth=queryoauth)
+            for tweet in corpus_r.json:
+                f.write( str(tweet)+'\n' )
+        page+=1
+    f.close()
 
 def read_tweets():
-    for line in fileinput.input():
+    f = open('corpus.json', 'r')
+    for line in f:
         dict_line = ast.literal_eval(line)
         yield dict_line
 
@@ -96,6 +130,8 @@ class Qualitweet(object):
         #print self.user_dict
         
 if __name__ == '__main__':
+    crawl()
+	
     tweets = read_tweets()
     
     algorithm = Qualitweet()
